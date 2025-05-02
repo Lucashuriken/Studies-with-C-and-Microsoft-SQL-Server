c3e3203c1f81d0664ea59055d771ff8042d2d6c7
LucasH <169024375+Lucashuriken@users.noreply.github.com>
2025-05-02 13:18:41 -0300

    Add files via upload

---
+++ Saving Employees info into a database sample/Form1.Designer.cs
@@ -0,0 +1,459 @@
+﻿namespace Cadastro_clientes_yt
+{
+    partial class Departamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nome = new System.Windows.Forms.Label();
+            this.cadastro = new System.Windows.Forms.Label();
+            this.cadastroText = new System.Windows.Forms.TextBox();
+            this.nomeText = new System.Windows.Forms.TextBox();
+            this.cpf = new System.Windows.Forms.Label();
+            this.rg = new System.Windows.Forms.Label();
+            this.endereço = new System.Windows.Forms.Label();
+            this.cidade = new System.Windows.Forms.Label();
+            this.cidadeText = new System.Windows.Forms.TextBox();
+            this.enderecoText = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.departamentoText = new System.Windows.Forms.TextBox();
+            this.datanascimento = new System.Windows.Forms.Label();
+            this.obs = new System.Windows.Forms.Label();
+            this.obsText = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.bairro = new System.Windows.Forms.Label();
+            this.bairroText = new System.Windows.Forms.TextBox();
+            this.genero = new System.Windows.Forms.Label();
+            this.masculino = new System.Windows.Forms.RadioButton();
+            this.feminino = new System.Windows.Forms.RadioButton();
+            this.laele = new System.Windows.Forms.RadioButton();
+            this.ativo = new System.Windows.Forms.CheckBox();
+            this.cpfText = new System.Windows.Forms.MaskedTextBox();
+            this.telefoneText = new System.Windows.Forms.MaskedTextBox();
+            this.nascimentoText = new System.Windows.Forms.MaskedTextBox();
+            this.rgText = new System.Windows.Forms.TextBox();
+            this.estadoText = new System.Windows.Forms.ComboBox();
+            this.estado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nome
+            // 
+            this.nome.AutoSize = true;
+            this.nome.Location = new System.Drawing.Point(112, 33);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(35, 13);
+            this.nome.TabIndex = 0;
+            this.nome.Text = "Nome";
+            // 
+            // cadastro
+            // 
+            this.cadastro.AutoSize = true;
+            this.cadastro.Location = new System.Drawing.Point(115, 120);
+            this.cadastro.Name = "cadastro";
+            this.cadastro.Size = new System.Drawing.Size(49, 13);
+            this.cadastro.TabIndex = 1;
+            this.cadastro.Text = "Cadastro";
+            // 
+            // cadastroText
+            // 
+            this.cadastroText.Location = new System.Drawing.Point(115, 148);
+            this.cadastroText.Name = "cadastroText";
+            this.cadastroText.Size = new System.Drawing.Size(100, 20);
+            this.cadastroText.TabIndex = 2;
+            // 
+            // nomeText
+            // 
+            this.nomeText.Location = new System.Drawing.Point(112, 61);
+            this.nomeText.Name = "nomeText";
+            this.nomeText.Size = new System.Drawing.Size(100, 20);
+            this.nomeText.TabIndex = 2;
+            // 
+            // cpf
+            // 
+            this.cpf.AutoSize = true;
+            this.cpf.Location = new System.Drawing.Point(115, 190);
+            this.cpf.Name = "cpf";
+            this.cpf.Size = new System.Drawing.Size(27, 13);
+            this.cpf.TabIndex = 0;
+            this.cpf.Text = "CPF";
+            // 
+            // rg
+            // 
+            this.rg.AutoSize = true;
+            this.rg.Location = new System.Drawing.Point(115, 273);
+            this.rg.Name = "rg";
+            this.rg.Size = new System.Drawing.Size(23, 13);
+            this.rg.TabIndex = 1;
+            this.rg.Text = "RG";
+            this.rg.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // endereço
+            // 
+            this.endereço.AutoSize = true;
+            this.endereço.Location = new System.Drawing.Point(297, 33);
+            this.endereço.Name = "endereço";
+            this.endereço.Size = new System.Drawing.Size(53, 13);
+            this.endereço.TabIndex = 0;
+            this.endereço.Text = "Endereço";
+            // 
+            // cidade
+            // 
+            this.cidade.AutoSize = true;
+            this.cidade.Location = new System.Drawing.Point(297, 192);
+            this.cidade.Name = "cidade";
+            this.cidade.Size = new System.Drawing.Size(40, 13);
+            this.cidade.TabIndex = 1;
+            this.cidade.Text = "Cidade";
+            this.cidade.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // cidadeText
+            // 
+            this.cidadeText.Location = new System.Drawing.Point(300, 232);
+            this.cidadeText.Name = "cidadeText";
+            this.cidadeText.Size = new System.Drawing.Size(203, 20);
+            this.cidadeText.TabIndex = 2;
+            // 
+            // enderecoText
+            // 
+            this.enderecoText.Location = new System.Drawing.Point(297, 61);
+            this.enderecoText.Name = "enderecoText";
+            this.enderecoText.Size = new System.Drawing.Size(206, 20);
+            this.enderecoText.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(569, 32);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Telefone";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(566, 99);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Departamento";
+            // 
+            // departamentoText
+            // 
+            this.departamentoText.Location = new System.Drawing.Point(569, 128);
+            this.departamentoText.Name = "departamentoText";
+            this.departamentoText.Size = new System.Drawing.Size(203, 20);
+            this.departamentoText.TabIndex = 2;
+            // 
+            // datanascimento
+            // 
+            this.datanascimento.AutoSize = true;
+            this.datanascimento.Location = new System.Drawing.Point(569, 273);
+            this.datanascimento.Name = "datanascimento";
+            this.datanascimento.Size = new System.Drawing.Size(104, 13);
+            this.datanascimento.TabIndex = 0;
+            this.datanascimento.Text = "Data de Nascimento";
+            // 
+            // obs
+            // 
+            this.obs.AutoSize = true;
+            this.obs.Location = new System.Drawing.Point(569, 176);
+            this.obs.Name = "obs";
+            this.obs.Size = new System.Drawing.Size(70, 13);
+            this.obs.TabIndex = 1;
+            this.obs.Text = "Observações";
+            // 
+            // obsText
+            // 
+            this.obsText.Location = new System.Drawing.Point(572, 216);
+            this.obsText.Multiline = true;
+            this.obsText.Name = "obsText";
+            this.obsText.Size = new System.Drawing.Size(203, 35);
+            this.obsText.TabIndex = 2;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(297, 357);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(52, 13);
+            this.label13.TabIndex = 1;
+            this.label13.Text = "Situação ";
+            // 
+            // bairro
+            // 
+            this.bairro.AutoSize = true;
+            this.bairro.Location = new System.Drawing.Point(297, 273);
+            this.bairro.Name = "bairro";
+            this.bairro.Size = new System.Drawing.Size(34, 13);
+            this.bairro.TabIndex = 1;
+            this.bairro.Text = "Bairro";
+            // 
+            // bairroText
+            // 
+            this.bairroText.Location = new System.Drawing.Point(300, 302);
+            this.bairroText.Name = "bairroText";
+            this.bairroText.Size = new System.Drawing.Size(203, 20);
+            this.bairroText.TabIndex = 2;
+            // 
+            // genero
+            // 
+            this.genero.AutoSize = true;
+            this.genero.Location = new System.Drawing.Point(115, 360);
+            this.genero.Name = "genero";
+            this.genero.Size = new System.Drawing.Size(42, 13);
+            this.genero.TabIndex = 5;
+            this.genero.Text = "Genero";
+            // 
+            // masculino
+            // 
+            this.masculino.AutoSize = true;
+            this.masculino.Location = new System.Drawing.Point(112, 377);
+            this.masculino.Name = "masculino";
+            this.masculino.Size = new System.Drawing.Size(73, 17);
+            this.masculino.TabIndex = 6;
+            this.masculino.TabStop = true;
+            this.masculino.Text = "Masculino";
+            this.masculino.UseVisualStyleBackColor = true;
+            // 
+            // feminino
+            // 
+            this.feminino.AutoSize = true;
+            this.feminino.Location = new System.Drawing.Point(112, 400);
+            this.feminino.Name = "feminino";
+            this.feminino.Size = new System.Drawing.Size(67, 17);
+            this.feminino.TabIndex = 7;
+            this.feminino.TabStop = true;
+            this.feminino.Text = "Feminino";
+            this.feminino.UseVisualStyleBackColor = true;
+            // 
+            // laele
+            // 
+            this.laele.AutoSize = true;
+            this.laele.Location = new System.Drawing.Point(112, 423);
+            this.laele.Name = "laele";
+            this.laele.Size = new System.Drawing.Size(54, 17);
+            this.laele.TabIndex = 8;
+            this.laele.TabStop = true;
+            this.laele.Text = "La ele";
+            this.laele.UseVisualStyleBackColor = true;
+            // 
+            // ativo
+            // 
+            this.ativo.AutoSize = true;
+            this.ativo.Location = new System.Drawing.Point(300, 377);
+            this.ativo.Name = "ativo";
+            this.ativo.Size = new System.Drawing.Size(50, 17);
+            this.ativo.TabIndex = 9;
+            this.ativo.Text = "Ativo";
+            this.ativo.UseVisualStyleBackColor = true;
+            // 
+            // cpfText
+            // 
+            this.cpfText.Location = new System.Drawing.Point(115, 216);
+            this.cpfText.Mask = "000.000.000-00";
+            this.cpfText.Name = "cpfText";
+            this.cpfText.Size = new System.Drawing.Size(100, 20);
+            this.cpfText.TabIndex = 10;
+            // 
+            // telefoneText
+            // 
+            this.telefoneText.Location = new System.Drawing.Point(569, 60);
+            this.telefoneText.Mask = "(00) 00000-0000";
+            this.telefoneText.Name = "telefoneText";
+            this.telefoneText.Size = new System.Drawing.Size(100, 20);
+            this.telefoneText.TabIndex = 11;
+            // 
+            // nascimentoText
+            // 
+            this.nascimentoText.Location = new System.Drawing.Point(572, 301);
+            this.nascimentoText.Mask = "00/00/0000";
+            this.nascimentoText.Name = "nascimentoText";
+            this.nascimentoText.Size = new System.Drawing.Size(100, 20);
+            this.nascimentoText.TabIndex = 12;
+            // 
+            // rgText
+            // 
+            this.rgText.Location = new System.Drawing.Point(115, 302);
+            this.rgText.Name = "rgText";
+            this.rgText.Size = new System.Drawing.Size(100, 20);
+            this.rgText.TabIndex = 2;
+            // 
+            // estadoText
+            // 
+            this.estadoText.FormattingEnabled = true;
+            this.estadoText.Items.AddRange(new object[] {
+            "AC",
+            "AL",
+            "AP",
+            "AM",
+            "BA",
+            "CE",
+            "DF",
+            "ES",
+            "GO",
+            "MA",
+            "MT",
+            "MS",
+            "MG",
+            "PA",
+            "PB",
+            "PR",
+            "PE",
+            "PI",
+            "RJ",
+            "RN",
+            "RS",
+            "RO",
+            "RR",
+            "SC",
+            "SP",
+            "SE",
+            "TO"});
+            this.estadoText.Location = new System.Drawing.Point(300, 148);
+            this.estadoText.Name = "estadoText";
+            this.estadoText.Size = new System.Drawing.Size(121, 21);
+            this.estadoText.TabIndex = 13;
+            // 
+            // estado
+            // 
+            this.estado.AutoSize = true;
+            this.estado.Location = new System.Drawing.Point(300, 120);
+            this.estado.Name = "estado";
+            this.estado.Size = new System.Drawing.Size(40, 13);
+            this.estado.TabIndex = 14;
+            this.estado.Text = "Estado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(650, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 39);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(512, 441);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 39);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Departamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cadastro_clientes_yt.Properties.Resources.gradiente_pro_form;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.estado);
+            this.Controls.Add(this.estadoText);
+            this.Controls.Add(this.nascimentoText);
+            this.Controls.Add(this.telefoneText);
+            this.Controls.Add(this.cpfText);
+            this.Controls.Add(this.ativo);
+            this.Controls.Add(this.laele);
+            this.Controls.Add(this.feminino);
+            this.Controls.Add(this.masculino);
+            this.Controls.Add(this.genero);
+            this.Controls.Add(this.enderecoText);
+            this.Controls.Add(this.rgText);
+            this.Controls.Add(this.nomeText);
+            this.Controls.Add(this.rg);
+            this.Controls.Add(this.cidadeText);
+            this.Controls.Add(this.bairroText);
+            this.Controls.Add(this.bairro);
+            this.Controls.Add(this.cidade);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.departamentoText);
+            this.Controls.Add(this.obsText);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.obs);
+            this.Controls.Add(this.datanascimento);
+            this.Controls.Add(this.cpf);
+            this.Controls.Add(this.endereço);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cadastroText);
+            this.Controls.Add(this.cadastro);
+            this.Controls.Add(this.nome);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Departamento";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "first blood";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nome;
+        private System.Windows.Forms.Label cadastro;
+        private System.Windows.Forms.TextBox cadastroText;
+        private System.Windows.Forms.TextBox nomeText;
+        private System.Windows.Forms.Label cpf;
+        private System.Windows.Forms.Label rg;
+        private System.Windows.Forms.Label endereço;
+        private System.Windows.Forms.Label cidade;
+        private System.Windows.Forms.TextBox cidadeText;
+        private System.Windows.Forms.TextBox enderecoText;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox departamentoText;
+        private System.Windows.Forms.Label datanascimento;
+        private System.Windows.Forms.Label obs;
+        private System.Windows.Forms.TextBox obsText;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label bairro;
+        private System.Windows.Forms.TextBox bairroText;
+        private System.Windows.Forms.Label genero;
+        private System.Windows.Forms.RadioButton masculino;
+        private System.Windows.Forms.RadioButton feminino;
+        private System.Windows.Forms.RadioButton laele;
+        private System.Windows.Forms.CheckBox ativo;
+        private System.Windows.Forms.MaskedTextBox cpfText;
+        private System.Windows.Forms.MaskedTextBox telefoneText;
+        private System.Windows.Forms.MaskedTextBox nascimentoText;
+        private System.Windows.Forms.TextBox rgText;
+        private System.Windows.Forms.ComboBox estadoText;
+        private System.Windows.Forms.Label estado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
